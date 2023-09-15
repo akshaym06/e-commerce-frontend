f6352a4dabe6e93b6d642eafef5b4b0a871b695e
Akshay <akshaymnavada6@gmail.com>
2023-09-15 21:01:44 +0530

    Show images to User(Image bytes to file)
    Show images to User(Image bytes to file)

--- src/app/show-product-details/show-product-details.component.ts
+++ src/app/show-product-details/show-product-details.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { ProductService } from '../_services/product.service';
 import { Product } from '../_model/product.model';
 import { HttpErrorResponse } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { ShowProductImagesDialogComponent } from '../show-product-images-dialog/show-product-images-dialog.component';
+import { ImageProcessingService } from '../image-processing.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-show-product-details',
@@ -12,23 +16,29 @@ export class ShowProductDetailsComponent {
 
   productDetails: Product[] = []
 
-  displayedColumns: string[] = ['Id', 'Product Name', 'Product Description', 'Product Discounted Price', 'Product Actual Price', 'Edit', 'Delete'];
+  displayedColumns: string[] = ['Id', 'Product Name', 'Product Description', 'Product Discounted Price', 'Product Actual Price', 'Images', 'Edit', 'Delete'];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    public imagesDialog: MatDialog,
+    private imageProcessingService: ImageProcessingService) { }
 
   ngOnInit(): void {
     this.getAllProducts();
   }
 
   public getAllProducts() {
-    this.productService.getAllProducts().subscribe({
-      next: (response: Product[]) => {
-        this.productDetails = response
-      },
-      error: (error: HttpErrorResponse) => {
-        console.log(error);
-      }
-    });
+    this.productService.getAllProducts()
+      .pipe(
+        map((x: Product[], i) => x.map((product: Product) => this.imageProcessingService.createImages(product)))
+      )
+      .subscribe({
+        next: (response: Product[]) => {
+          this.productDetails = response
+        },
+        error: (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      });
   }
 
   public deleteProduct(productId) {
@@ -39,6 +49,18 @@ export class ShowProductDetailsComponent {
       error: (error: HttpErrorResponse) => {
         console.log(error);
       }
+    });
+  }
+
+  public showImages(product: Product) {
+    console.log(product);
+
+    this.imagesDialog.open(ShowProductImagesDialogComponent, {
+      data:{
+        images: product.productImages
+      },
+      height: '500px',
+      width: '800px'
     });
   }
 }
